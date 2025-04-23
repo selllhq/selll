b26637bec2751c58ac5f65c51facf7c93d7a70d6
mychidarko <mickdd22@gmail.com>
2025-04-23 12:27:42 +0000

    feat: install analytics

--- app/views/_inertia.blade.php
+++ app/views/_inertia.blade.php
@@ -9,6 +9,19 @@
     @viteReactRefresh
     @vite(['/js/app.jsx', "/js/pages/{$page['component']}.jsx"])
     @inertiaHead
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-28PQM4WL3L"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+        gtag('config', 'G-28PQM4WL3L');
+    </script>
 </head>
 
 <body>
