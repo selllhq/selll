529298cceacbe6adb94f07cb0b7047e7a1df0be9
mychidarko <mickdd22@gmail.com>
2025-05-05 10:58:46 +0000

    fix: patch up font issue

--- app/views/_inertia.blade.php
+++ app/views/_inertia.blade.php
@@ -6,6 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="https://zero.leafphp.dev/assets/img/logo.png" type="image/x-icon">
     <title inertia>Selll - The easiest way to sell online</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,200..800&amp;display=swap"
+      rel="stylesheet"
+    />
     @viteReactRefresh
     @vite(['/js/app.jsx', "/js/pages/{$page['component']}.jsx"])
     @inertiaHead
