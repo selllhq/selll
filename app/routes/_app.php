edb5aa3ad0e6a11d96c304a277af379f5bf8ac4e
mychidarko <mickdd22@gmail.com>
2025-05-19 17:25:17 +0000

    chore: clean routes up

--- app/routes/_app.php
+++ app/routes/_app.php
@@ -2,18 +2,7 @@
 
 app()->get('/', 'HomeController@index');
 app()->inertia('/pricing', 'pricing');
-
-
-
 app()->inertia('/faqs', 'faqs');
-
-
-
 app()->inertia('/privacy', 'privacy');
-
-
 app()->inertia('/terms', 'terms');
-
-
-
 app()->inertia('/contact', 'contact');
