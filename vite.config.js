8f08c59dc0267d533c59c73b25048b8c208c336a
mychidarko <mickdd22@gmail.com>
2025-05-30 14:29:50 +0000

    chore: setup eslint

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,25 @@
-import { defineConfig } from 'vite';
-import leaf from '@leafphp/vite-plugin';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import leaf from "@leafphp/vite-plugin";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
         leaf({
-            input: ['app/views/js/app.jsx'],
+            input: ["app/views/js/app.jsx"],
             refresh: true,
         }),
         react(),
+        eslintPlugin({
+            cache: false,
+            include: ["./app/views/js/**/*.js", "./app/views/js/**/*.jsx"],
+            exclude: [],
+        }),
     ],
     resolve: {
         alias: {
-            '@': '/app/views/js',
+            "@": "/app/views/js",
         },
     },
 });
