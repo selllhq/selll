53140a2f6c862a978c6bca65b5690cf555b0f603
mychidarko <mickdd22@gmail.com>
2025-06-21 11:18:37 +0000

    chore: move noscript above page content

--- app/views/_inertia.blade.php
+++ app/views/_inertia.blade.php
@@ -66,12 +66,12 @@
 </head>
 
 <body>
-    @inertia
-
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W7TC9W5S" height="0" width="0"
             style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
+
+    @inertia
 </body>
 
 </html>
