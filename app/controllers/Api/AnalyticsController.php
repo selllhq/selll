4214c2840bf799da3974a5db293f17572671c6be
mychidarko <mickdd22@gmail.com>
2025-07-07 18:17:05 +0000

    chore: switch to no-content analytics

--- app/controllers/Api/AnalyticsController.php
+++ app/controllers/Api/AnalyticsController.php
@@ -89,7 +89,7 @@ class AnalyticsController extends Controller
             'user_device' => $data['user_device'],
         ]);
 
-        return response()->json($geoData);
+        return response()->noContent();
     }
 
     public function addToCart()
