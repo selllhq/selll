04865ae3861d50fec73771598b6e33af6cd31995
mychidarko <mickdd22@gmail.com>
2025-07-23 22:24:36 +0000

    feat: add store url update for mobile

--- app/routes/_mobile.php
+++ app/routes/_mobile.php
@@ -1,9 +1,24 @@
 <?php
 
-app()->post('/mobile/auth/login', 'Mobile\AuthController@index');
+app()->post('/api/mobile/auth/login', 'Mobile\AuthController@index');
+app()->get('/api/mobile/stats', 'Mobile\StatsController@index');
 
-app()->group('/mobile', function () {
-    app()->get('/stats', 'Mobile\StatsController@index');
-    app()->get('/orders', 'Mobile\OrdersController@index');
-    app()->get('/orders/(\d+)', 'Mobile\OrdersController@show');
+app()->group('/api/mobile/orders', function () {
+    app()->get('/', 'Mobile\OrdersController@index');
+    app()->get('/(\d+)', 'Mobile\OrdersController@show');
+    app()->post('/(\d+)/shipping', 'Mobile\OrdersController@shipping');
+    app()->post('/(\d+)/cancel', 'Mobile\OrdersController@cancel');
+    app()->post('/(\d+)/complete', 'Mobile\OrdersController@complete');
+});
+
+app()->group('/api/mobile/products', function () {
+    app()->get('/', 'Mobile\ProductsController@index');
+    app()->post('/new', 'Mobile\ProductsController@store');
+    app()->get('/(\d+)', 'Mobile\ProductsController@show');
+    app()->post('/(\d+)/edit', 'Mobile\ProductsController@update');
+    app()->delete('/(\d+)', 'Mobile\ProductsController@destroy');
+});
+
+app()->group('/api/mobile/store', function () {
+    app()->post('/domain', 'Mobile\StoreController@domain');
 });
