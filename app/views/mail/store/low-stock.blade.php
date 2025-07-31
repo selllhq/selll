d3ad3cd7751276ef2db0995e6f18da99d59ae049
mychidarko <mickdd22@gmail.com>
2025-07-31 22:47:05 +0000

    feat: add low-stock mail/sms

---
+++ app/views/mail/store/low-stock.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Low Stock</title>
+    <meta name="robots" content="noindex, nofollow">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+    <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,200..800&amp;display=swap"
+        rel="stylesheet" />
+</head>
+
+<body
+    style="box-sizing: border-box; position: relative; font-family: 'Bricolage Grotesque', sans-serif; font-size: 16px; -webkit-text-size-adjust: none; background-color:
+    #F7F7F8; color: #25252D; height: 100%; line-height: 2; margin: 0; padding: 0; width: 100% !important;
+    -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale;">
+
+    <table class="wrapper" width="100%" cellpadding="0" cellspacing="0" role="presentation"
+        style="box-sizing: border-box; position: relative;font-family: 'Bricolage Grotesque', sans-serif; font-size: 16px; line-height: 2;
+        -premailer-cellpadding: 0; -premailer-cellspacing:0; -premailer-width: 100%; background-color: #F7F7F8; margin:
+        0; padding: 0; width: 100%;">
+        <tr>
+            <td align="center"
+                style="box-sizing: border-box; position: relative; font-family: 'Bricolage Grotesque', sans-serif;
+                font-size: 16px; line-height: 2;">
+                <table class="content" width="100%" cellpadding="0" cellspacing="0" role="presentation"
+                    style="box-sizing: border-box; position: relative; font-family: 'Bricolage Grotesque', sans-serif; font-size: 16px; line-height:
+                    2; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; margin: 0;
+                    padding: 0; width: 100%;">
+                    <tr>
+                        <td class="header" width="100%" cellpadding="0" cellspacing="0"
+                            style="box-sizing:
+                            border-box; position: relative; font-family: 'Bricolage Grotesque', sans-serif;
+                            font-size: 16px; line-height: 2;">
+                            <table class="inner-header" align="center" width="464" cellpadding=
+"0" cellspacing="0"
+                                role="presentation"
+                                style="box-sizing: border-box; position:
+                                relative; font-family: 'Bricolage Grotesque', sans-serif; font-size: 16px;
+                                line-height: 2; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width:
+                                456px; background-color: #F7F7F8;border-color: #e8e5ef; border-radius: 2px;
+                                border-width: 1px; margin: 0 auto; padding: 0; width: 456px;">
+                                <tr>
+                                    <td class="content-cell"
+                                        style="box-sizing: border-box; position: relative;
+                                        font-family: 'Bricolage Grotesque', sans-serif; font-size: 16px; line-height: 2; max-width: 100vw; padding-left: 16px;
+                                        padding-right: 16px; padding-top: 56px; padding-bottom: 40px;">
+                                        <a href="https://selll.online"
+                                            style="box-sizing: border-box; position: relative; font-family:
+                                            'Bricolage Grotesque', sans-serif; font-size: 16px;
+                                            line-height:2; color: #3d4852; font-weight: 600; text-decoration: none;
+                                            display: inline-block;">
+                                            <img src="https://selll.online/assets/img/logo.png" class="logo"
+                                                alt="Selll"
+                                                style="box-sizing: border-box;
+                                                position:relative; font-family: 'Bricolage Grotesque', sans-serif;font-size: 16px; line-height: 2; max-width:
+                                                100%; border: none; width: 30px; height: 50px;">
+                                        </a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td class="body" width="100%" cellpadding="0" cellspacing="0"
+                            style="box-sizing:
+                            border-box; position: relative; font-family: 'Bricolage Grotesque', sans-serif; font-size:
+                            16px;line-height: 2; -premailer-cellpadding: 0; -premailer-cellspacing: 0;
+                            -premailer-width: 100%; background-color: #F7F7F8; border-bottom: 1px solid #F7F7F8;
+                            border-top: 1px solid #F7F7F8; margin: 0; padding: 0; width: 100%;">
+                            <table class="inner-body" align="center" width="456" cellpadding="0" cellspacing="0"
+                                role="presentation"
+                                style=
+"box-sizing: border-box; position: relative; font-family: 'Bricolage Grotesque', sans-serif; font-size: 16px; line-height: 2; -premai
+ler-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 456px; bac
+kground-color: #F7F7F8; border-color: #e8e5ef; border-radius: 2px; border-w
+idth: 1px; margin: 0 auto; padding: 0; width: 456px;">
+                                <tr>
+                                    <td class="content-cell"
+                                        style="box-sizing: border-box; position: relative;
+                                        font-family: 'Bricolage Grotesque', sans-serif; font-size: 16px; line-height: 2; max-width: 100vw; padding-left: 16px;
+                                        padding-right: 16px;">
+                                        <h1
+                                            style="box-sizing: border-box; position: relative; font-family: 'Bricolage Grotesque', sans-serif; font-weight: 500;
+                                            line-height: 1.5; margin-bottom: 24px; font-size:24px; margin-top: 0;
+                                            text-align: left;">
+                                            {{ $product->name }} is almost out of stock
+                                        </h1>
+                                        <p
+                                            style="box-sizing: border-box; position: relative; font-family:
+                                            'Bricolage Grotesque', sans-serif; font-size: 16px;
+                                            line-height:28px; margin-bottom: 24px; margin-top: 0; text-align: left;">
+                                            You have only {{ $product->quantity_items }} items left in stock. Your
+                                            customers
+                                            seem to love this product, and we want to ensure they can continue to
+                                            purchase it without any interruptions.
+                                        </p>
+                                        <p
+                                            style="box-sizing: border-box; position: relative; font-family:
+                                            'Bricolage Grotesque', sans-serif; font-size: 16px;
+                                            line-height:28px; margin-bottom: 24px; margin-top: 0; text-align: left;">
+                                            If you have more stock available, please update your dashboard to ensure
+                                            your
+                                            customers can continue to purchase this product.
+                                        </p>
+                                        <p
+                                            style="box-sizing: border-box; position: relative; font-family:
+                                            'Bricolage Grotesque', sans-serif; font-size: 16px;
+                                            line-height:28px; margin-bottom: 24px; margin-top: 0; text-align: left;">
+                                            Cheers,<br>
+                                            Ashley from Selll
+                                        </p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td
+                            style="box-sizing: border-box; position: relative; font-family: 'Bricolage Grotesque', sans-serif;
+                            font-size: 16px; line-height: 2;">
+                            <table class="footer" align="center" width="464" cellpadding="0" cellspacing="0"
+                                role="presentation"
+                                style="box-sizing: border-box; position: relative; font-family:
+                                'Bricolage Grotesque', sans-serif; font-size: 16px; line-height: 2; -premailer-cellpadding:
+                                0; -premailer-cellspacing: 0; -premailer-width: 456px; margin: 0 auto; padding: 0;
+                                width: 456px;">
+                                <tr>
+                                    <td class="content-cell" align="center"
+                                        style="box-sizing: border-box;
+                                        position: relative; font-family: 'Bricolage Grotesque', sans-serif; font-size: 16px; line-height: 2; max-width: 100vw; padding-left:
+                                        16px; padding-right: 16px;">
+                                        <p
+                                            style="box-sizing: border-box; position: relative; font-family:
+                                            'Bricolage Grotesque', sans-serif; margin-bottom:
+                                            24px; margin-top: 0; text-align: left; color: #6C6C84; font-size: 14px;
+                                            line-height: 24px;">
+                                            Copyright 2025 - Selll, LTD.<br>
+                                            How fast can you selll?</p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
