437db994355bd8e664ed0900da30de8344ae6370
mychidarko <mickdd22@gmail.com>
2025-08-10 12:01:26 +0000

    feat: update delivery

--- app/controllers/Api/StoresController.php
+++ app/controllers/Api/StoresController.php
@@ -10,7 +10,7 @@ class StoresController extends Controller
 {
     public function show($store)
     {
-        $store = Store::where('slug', $store)->with(['owner', 'deliveryDefaults'])->first();
+        $store = Store::where('slug', $store)->with(['owner'])->first();
 
         if (!$store) {
             return response()->json([
